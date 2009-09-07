8e63d01395ec03839deabe72bfdc09435f9ea0e9
Jeremy McNevin <jeremy@fastpencil.com>
2009-09-07 16:05:03 -0700

    Finishing up first iteration.

--- public/javascripts/default.js
+++ public/javascripts/default.js
@@ -3,4 +3,9 @@ var email_host = 'smallclaimscourtlawyer.com';
 $(document).ready(function() {
   var email = email_name + '@' + email_host;
   $("#contact_email_link").attr('href','mailto:'+email).html(email);
+  
+  $("#contact_form").validate();
+  
+  $("#date").datepicker();
 });
+
--- sccl.rb
+++ sccl.rb
@@ -3,27 +3,33 @@ require 'vendor/sinatra/lib/sinatra.rb'
 require 'vendor/pony/lib/pony.rb'
 require 'erb'
 
+CONTACT_FORM_EMAIL = 'jmcnevin@gmail.com'
+CONTACT_FORM_SUBJECT = 'Contact request from SmallClaimsCourtLawyer.com'
+
 get '/' do
   erb :index
 end
 
 get '/contact' do
+  @page_title = 'Contact'
   erb :contact
 end
 
 post '/contact' do
-  # order@attorneysforcourt.com
-  Pony.mail :to => 'jmcnevin@gmail.com',
+  @page_title = 'Contact'
+  Pony.mail :to => CONTACT_FORM_EMAIL,
     :from => params[:email],
-    :subject => 'Contact request from SmallClaimsCourtLawyer.com',
-    :body => erb(:email)
+    :subject => CONTACT_FORM_SUBJECT,
+    :body => erb(:email, :layout => false)
   erb :success
 end
 
 get '/about' do
+  @page_title = 'About'
   erb :about
 end
 
 get '/payments' do
+  @page_title = 'Get Started / Payments'
   erb :payments
 end
